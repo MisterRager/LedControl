52743f6c6809189167a84ebd178c6f9d8a08d10d
Alan James Rager <guy@books.lickmy.rocks>
2015-01-25 19:33:22 -0800

    first iteration - this script takes a strip length and pattern and pushes them to the connected strip through the mbed firmware

---
+++ control.rb
@@ -0,0 +1,26 @@
+require 'rubygems'  
+require 'rubyserial'
+
+#params for serial port  
+#port_str = "/dev/ttyUSB0"  #may be different for you  
+port_str = "/dev/ttyACM4"  #may be different for you  
+baud_rate = 2000000
+
+sp = Serial.new( port_str, baud_rate )
+
+len = ARGV[0].to_i * 3
+pattern = ARGV[1]
+
+out = ""
+
+while out.length < len do
+	out << pattern
+end
+
+out = out[ 0, len ]
+
+while true do
+	sp.write "#{out}\r\n"
+	out = (out[ -3, 3 ] + out)[0, len]
+	sleep 0.02
+end
